8d7b108fd956149790346f5426116a4c28487aa3
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2009-04-08 08:36:37 +0000

    Import of TOKUHIROM/Moxy-0.56 from CPAN.
    gitpan-cpan-distribution: Moxy
    gitpan-cpan-version:      0.56
    gitpan-cpan-path:         TOKUHIROM/Moxy-0.56.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Moxy.pm
+++ lib/Moxy.pm
@@ -5,7 +5,7 @@ use warnings;
 use base qw/Class::Accessor::Fast/;
 use Class::Component 0.16;
 
-our $VERSION = '0.55';
+our $VERSION = '0.56';
 
 use Carp;
 use Encode;
@@ -45,7 +45,7 @@ __PACKAGE__->load_components(qw/Plaggerize Autocall::InjectMethod Context/);
 
 __PACKAGE__->load_plugins(qw/
     DisplayWidth ControlPanel LocationBar Pictogram
-    BasicAuth InternalServerError
+    Status::401 Status::500 Status::404
     UserID XMLisHTML UserAgentSwitcher RefererCutter CookieCutter FlashUseImgTag
     DisableTableTag GPS HTTPHeader QRCode ShowHTTPHeaders
 /);
--- lib/Moxy/Plugin/Status/404.pm
+++ lib/Moxy/Plugin/Status/404.pm
@@ -1,4 +1,4 @@
-package Moxy::Plugin::InternalServerError;
+package Moxy::Plugin::Status::404;
 use strict;
 use warnings;
 use base qw/Moxy::Plugin/;
@@ -9,14 +9,14 @@ sub status_handler : Hook {
     my $response = $args->{response};
 
     # handle internal server error
-    # do not display f*cking plain html page when got a 500.
-    if ($response->code eq 500 && $response->content !~ /<body>/) {
+    # do not display f*cking plain html page when got a 404.
+    if ($response->code eq 404 && $response->content !~ /<body>/) {
         $response->content_type('text/html');
         $response->content(qq{
             <html>
-                <head><title>internal server error occured</title></head>
+                <head><title>file not found occured</title></head>
                 <body>
-                    <div style="color: red; font-weight: bold; font-size: xx-large;">you got a 500 internal server error</div>
+                    <div style="color: yellow; background-color: black; font-weight: bold; font-size: xx-large;">you got a 'file not found'</div>
                     <div>@{[ $response->content ]}</div>
                     <div>-- moxy</div>
                 </body>
